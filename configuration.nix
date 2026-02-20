a193b76c8986a6a2f401a995f9c480914cab23e4
azabase <azizinte@gmail.com>
2026-02-21 01:29:22 +0500

    sys: keep git/curl/wget as rescue tools

--- configuration.nix
+++ configuration.nix
@@ -86,6 +86,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+  git
+  curl
+  wget
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
