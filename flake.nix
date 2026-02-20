e6b2c5c05f8ed49a795158388c8f2cd82263cff3
azabase <azizinte@gmail.com>
2026-02-21 00:03:05 +0500

    flake: add home-manager + home/buliway.nix (test: hello)

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "NixOS flake for host nixos (user: buliway)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+      ];
+    };
+  };
+}
---
+++ home/buliway.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+{
+  home.username = "buliway";
+  home.homeDirectory = "/home/buliway";
+  home.stateVersion = "25.11";
+
+  programs.git.enable = true;
+
+  # пока только тестовый пакет, чтобы проверить, что HM реально работает
+  home.packages = with pkgs; [
+    hello
+  ];
+}
