33de8411baae1bf27580410f115fcf7495531005
azabase <azizinte@gmail.com>
2026-02-22 10:28:19 +0500

    Fix duplicate package entries in configuration.nix
    Removed duplicate entries for git, curl, and wget in systemPackages.

--- configuration.nix
+++ configuration.nix
@@ -86,9 +86,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  git
-  curl
-  wget
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
